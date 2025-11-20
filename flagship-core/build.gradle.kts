a7b8a7fb06f886c5217b59bfc551130edfd2eaca
maxluxs <max.developer.luxs@gmail.com>
2025-11-20 23:37:52 +0300

    Add SPM and Dokka plugins to build
    - Apply Dokka plugin to root and flagship-core
    - Configure multiplatformSwiftPackage for iOS
    - Set up dokka output directory

---
+++ flagship-core/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.multiplatformSwiftPackage)
+    alias(libs.plugins.dokka)
+    // Publishing: uncomment when ready to publish
+    // `maven-publish`
+    // signing
+}
+
+// apply(from = rootProject.file("gradle/publish.gradle.kts"))
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+                }
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "FlagshipCore"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization.json)
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+
+        androidMain.dependencies {
+        }
+
+        iosMain.dependencies {
+        }
+    }
+}
+
+multiplatformSwiftPackage {
+    packageName("Flagship")
+    swiftToolsVersion("5.3")
+    targetPlatforms {
+        iOS { v("14") }
+    }
+}
+
+android {
+    namespace = "io.maxluxs.flagship.core"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
