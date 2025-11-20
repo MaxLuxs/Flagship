90d96c38be5f9eea11e2a27ccb62880bebdc0d3f
maxluxs <max.developer.luxs@gmail.com>
2025-11-20 23:37:52 +0300

    Add SPM and Dokka plugins to build
    - Apply Dokka plugin to root and flagship-core
    - Configure multiplatformSwiftPackage for iOS
    - Set up dokka output directory

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.composeHotReload) apply false
+    alias(libs.plugins.composeMultiplatform) apply false
+    alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.multiplatformSwiftPackage) apply false
+    alias(libs.plugins.dokka)
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>().configureEach {
+    moduleName.set("Flagship")
+    outputDirectory.set(file("$rootDir/docs"))
+}
