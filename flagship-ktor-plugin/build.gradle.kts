058981e4c787220bdb79b4bc4a752c6a3ebf8027
maxluxs <max.developer.luxs@gmail.com>
2025-11-22 17:23:19 +0300

    feat: add Ktor plugin module

---
+++ flagship-ktor-plugin/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    alias(libs.plugins.kotlinSerialization)
+    id("java-library")
+}
+
+group = "io.maxluxs.flagship"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api(projects.flagshipCore)
+    
+    // Ktor Server
+    compileOnly(libs.ktor.server.core)
+    
+    // Testing
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test)
+}
+
